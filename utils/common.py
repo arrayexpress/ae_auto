15270e8803d076b1c7a4dcf3ce33a7aef3f3c8c8
Ahmed Ali <ahmed@ebi.ac.uk>
2017-09-01 18:20:23 +0100

    executing commands as other users

--- utils/common.py
+++ utils/common.py
@@ -1,13 +1,30 @@
 from subprocess import Popen, PIPE
 
+import pwd
+
+import os
+from nbstreamreader import NonBlockingStreamReader as NBSR
+
 __author__ = 'Ahmed G. Ali'
 
 
 def execute_command(cmd, user=None):
-    become_user = ''
     if user:
-        become_user = ''
+        cmd = "sudo -H -u %s bash -c '%s'" % (user, cmd)
 
     p = Popen([cmd], stdout=PIPE, stderr=PIPE, shell=True)
     out, err = p.communicate()
     return out, err
+
+
+if __name__ == '__main__':
+    out, err =  execute_command('ssh oy-ena-login-1 "cd /fire/staging/aexpress; ls E-MTAB-5481"', 'fg_cur',)
+    for i in out.split('\n'):
+        print i
+    print '=' *50
+    for i in err.split('\n'):
+        print i
+    print '=' *50
+    # out, err =  execute_command('cd; pwd; ls -lah', 'gemmy',)
+    # for i in out.split('\n'):
+    #     print i
